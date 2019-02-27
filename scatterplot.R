5b9f4bd0615b727b568f3d2edde5d93616f4bd37
Josh Harrison <jgharrison1982@gmail.com>
2019-02-27 16:21:24 -0700

    first commit

---
+++ scatterplot.R
@@ -0,0 +1,11 @@
+dat <- read.csv("./data/testdata.csv")
+
+pdf(width = 8, 
+    height = 8,
+    file = "./results/scatterplot.pdf")
+plot(dat$rnorm.100..100.,
+     dat$rnorm.100..10.,
+     main = "",
+     xlab = "fake data",
+     ylab = "more fake data")
+dev.off()
