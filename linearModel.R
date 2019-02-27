da4479321709d39421c59b49827c9b8797b7b97f
Josh Harrison <jgharrison1982@gmail.com>
2019-02-27 16:33:18 -0700

    added some extra crap to the table

--- linearModel.R
+++ linearModel.R
@@ -4,8 +4,16 @@ dat <- read.csv("./data/testdata.csv")
 
 reg <- lm(dat$rnorm.100..100. ~ dat$rnorm.100..10.)
 
+#NOTE that it is very helpful to label your tables and figures so that you can
+#reference them from anywhere in a document when in Latex and it will always
+#reference them by the proper order. No more checking through to make sure figure numbers
+#are correct after reording them!
+
 print(xtable(summary(reg)$coefficients, 
-      type = "latex"),
+        type = "latex",
+        caption = "Results from a regression of a simulated vector of deviates from a normal distribution centered at 100 against deviates from a normal distribution centered at 10. xtable is awesome!",
+        label = "table:lm_results",
+        digits = 2), #round to the correct number of digits automatically
       file = "./results/lm_results.tex",
       floating.environment='table', #Can change to sideways table if landscape format desired
       include.rownames = FALSE)
