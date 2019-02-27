2e64269220e2b008174f651d7823db19efc43d04
Josh Harrison <jgharrison1982@gmail.com>
2019-02-27 16:54:06 -0700

    cleaned up table output

--- linearModel.R
+++ linearModel.R
@@ -13,7 +13,13 @@ print(xtable(summary(reg)$coefficients,
         type = "latex",
         caption = "Results from a regression of a simulated vector of deviates from a normal distribution centered at 100 against deviates from a normal distribution centered at 10. xtable is awesome!",
         label = "table:lm_results",
-        digits = 2), #round to the correct number of digits automatically
+        digits = 2, #round to the correct number of digits automatically
+        align = rep("c", dim(summary(reg)$coefficients)[2] + 1) #align cell contents, has to be vector of length equal to the number of columns plus one
+        #You can add horizontal lines, etc. too. Basically, all the easy formatting stuff you do in Latex you can do here.
+        #If you have to build a really crazy table then you may need to copy the output of xtable into Latex and edit after the fact, or
+        #build a function to paste content into the output of xtable prior to printing. 
+        ),
+      caption.placement = "top",
       file = "./results/lm_results.tex",
       floating.environment='table', #Can change to sideways table if landscape format desired
       include.rownames = FALSE)
